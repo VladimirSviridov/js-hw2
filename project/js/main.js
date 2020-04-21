ed9493d69135b4d85d8930625e266c8468f1398e
Vladimir Sviridov <vladim.svir@yandex.ru>
2020-04-21 17:25:38 +0300

    Create styles, modified js function

--- project/js/main.js
+++ project/js/main.js
@@ -5,21 +5,54 @@ const products = [
     {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price, img = "https://placehold.it/100x100") => {
-    return `<div class="product-item">
+/**
+ * Функция, создающая строчный литерал, содержащий параметры продаваемого объекта
+ * Добавил в нее data атрибут и стандартные значения
+ * @param title
+ * @param price
+ * @param id
+ * @param img
+ * @returns {string}
+ */
+const renderProduct = (title = "The thing", price = "100500", id = "0",
+                       img = "https://placehold.it/150x150/333") => {
+    return `<div class="product-item" data-id = "${id}">
             <div><img src="${img}" alt=""></div>
             <h3>${title}</h3>
             <p>${price}</p>
-            <button class="by-btn">Добавить в корзину</button>
+            <button class="buy-btn">Добавить в корзину</button>
           </div>`;
 };
 
-const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price);
-    });
-    console.log(productList);
+//Нашел два схожих способа упрощения написания
+//Применил деструктуризацию массива products
+//Первый через цикл for
+/*let productList = [];
+const items = () => {
+    for (let i = 0; i < products.length; i++){
+        let {id, title, price} = products[i];
+        productList += renderProduct(title, price, id);
+    }
     document.querySelector('.products').innerHTML = productList;
 };
+items(products);*/
 
-renderProducts(products);
+//Второй через метод forEach
+let productList = [];
+products.forEach(product => {
+    let {id, title, price} = product;
+    productList += renderProduct(title, price, id);
+});
+document.querySelector('.products').innerHTML = productList;
+
+//К сожалению, я не понял, почему в данных случаях "," не появляется,
+//а в методе map из лекции появляется.
+
+
+/*const renderProducts = (list) => {
+    const productList = list.map((good) => {
+        return renderProduct(good.title, good.price, good.id);
+    });
+    document.querySelector('.products').innerHTML = productList;
+};
+renderProducts(products);*/
