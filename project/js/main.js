f6b274537b6d77fb82bc11a462a267abfceaa3c7
Vladimir Sviridov <vladim.svir@yandex.ru>
2020-04-26 14:19:05 +0300

    Create lesson 2 hw folder and insert code from lecture

--- project/js/main.js
+++ project/js/main.js
@@ -1,20 +1,82 @@
-const products = [
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this._render();
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ]
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`;
+    }
+}
+new ProductList();
+
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000},
+//   {id: 2, title: 'Mouse', price: 1500},
+//   {id: 3, title: 'Keyboard', price: 5000},
+//   {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item" data-id="${this.id}">
+//               <img src="${img}" alt="Some img">
+//               <div class="desc">
+//                   <h3>${item.title}</h3>
+//                   <p>${item.price} \u20bd</p>
+//                   <button class="buy-btn">Купить</button>
+//               </div>
+//           </div>`;
+//
+// const renderProducts = list => document.querySelector('.products')
+//     .insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
+//
+// renderProducts(products);
+
+/*const products = [
     {id: 1, title: 'Notebook', price: 20000},
     {id: 2, title: 'Mouse', price: 1500},
     {id: 3, title: 'Keyboard', price: 5000},
     {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-/**
- * Функция, создающая строчный литерал, содержащий параметры продаваемого объекта
- * Добавил в нее data атрибут и стандартные значения
- * @param title
- * @param price
- * @param id
- * @param img
- * @returns {string}
- */
-const renderProduct = (title = "The thing", price = "100500", id = "0",
+const renderProduct = (title = "The thing", price = 100500, id = "0",
                        img = "https://placehold.it/150x150/333") => {
     return `<div class="product-item" data-id = "${id}">
             <div><img src="${img}" alt=""></div>
@@ -23,21 +85,7 @@ const renderProduct = (title = "The thing", price = "100500", id = "0",
             <button class="buy-btn">Добавить в корзину</button>
           </div>`;
 };
-
-//Придумал два схожих способа упрощения написания
-//Применил деструктуризацию массива products
-//Первый через цикл for
-/*let productList = [];
-const items = () => {
-    for (let i = 0; i < products.length; i++){
-        let {id, title, price} = products[i];
-        productList += renderProduct(title, price, id);
-    }
-    document.querySelector('.products').innerHTML = productList;
-};
-items(products);*/
-
-//Второй через метод forEach
+/*
 let productList = '';
 const renderProducts = (products) => {
     products.forEach(product => {
@@ -47,16 +95,4 @@ const renderProducts = (products) => {
     document.querySelector('.products').insertAdjacentHTML('afterbegin', productList);
 };
 renderProducts(products);
-
-//Я так понял, в данных случаях "," не появляется, потому что у меня получилась строка
-//а в методе map из лекции появляется "," т.к. получается массив
-//Не понял надо ли оборачивать в функцию renderProducts и передавать в неё products - все работало и без этого.
-
-
-/*const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price, good.id);
-    });
-    document.querySelector('.products').innerHTML = productList;
-};
-renderProducts(products);*/
+*/
