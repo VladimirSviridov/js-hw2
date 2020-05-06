266a46c2312b3fc03ecc4e5d389198010e75db41
Vladimir Sviridov <vladim.svir@yandex.ru>
2020-05-06 10:02:20 +0300

    Take lesson 5 code and start to make HW5

--- Lesson4/#3/app.js
+++ Lesson4/#3/app.js
@@ -5,75 +5,40 @@ class Inputarea {
   constructor(btn, area) {
     this.btn = btn;
     this.area = area;
+    this.regex = {
+      //Буквы русские и английские, начинающиеся с заглавной буквы
+      name: /(^[A-Z]{1}[a-z]{1,14} [A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/gi,
+      //Телефонный номер в указанном формате
+      phone: /(^(?!\+.*\(.*\).*\-\-.*$)(?!\+.*\(.*\).*\-$)(\+[0-9]\([0-9]{3}\)[0-9]([-0-9]{3})?([0-9]{4})?)$)/gi,
+      //Почта в указанном формате
+      email: /^(?!.*\..*,.*\/.*\\-.*\+.*\\.*&.*\^.*%.*\$.*#.*@.*!.*&.*.*.*\(.*\).*$)([A-Z]|[a-z]|[А-Я]|[а-я]+)/gi,
+      //Все что угодно, кроме определенных символов, таких так: .,/\ и прочие.
+      //Не получилось сделать проверку на "непустоту" строки
+      inputText: /^(?!.*@.*@.*$)(?!.*@.*\-\-.*\..*$)(?!.*@.*\-\..*$)(?!.*@.*\-$)(.*@.+(\..{1,11})?)$/gi,
+    };
     //Сначала проверяем наполненность
-    this._isFull();
+    this._regExp();
   }
+
   //Метод присваивает input "красный" класс и выводит alert
-  _makeRed(){
+  _makeRed() {
     alert(`${this.area.id} - пустая или содержит запрещенные символы!`);
     this.area.classList.add('red');
   };
+
   //Приветные методы, проверящие поля на соответствие регулярным выражениям,
   //если true, то вызывается "окрашивающий" метод
-  _regExpName(){
-    //Буквы русские и английские, начинающиеся с заглавной буквы
-    const regex = /(^[A-Z]{1}[a-z]{1,14} [A-Z]{1}[a-z]{1,14}$)|(^[А-Я]{1}[а-я]{1,14} [А-Я]{1}[а-я]{1,14}$)/gi;
-    if (!regex.test(this.area.value)){
-      this._makeRed();
+  _regExp() {
+    for (let key in this.regex) {
+      if (key === this.area.id) {
+        const regex = this.regex[key];
+        if (!regex.test(this.area.value)) {
+          this._makeRed();
+          break;
+        }
+      }
     }
   };
-  _regExpPhone(){
-    //Телефонный номер в указанном формате
-    const regex = /(^(?!\+.*\(.*\).*\-\-.*$)(?!\+.*\(.*\).*\-$)(\+[0-9]\([0-9]{3}\)[0-9]([-0-9]{3})?([0-9]{4})?)$)/gi;
-    if (!regex.test(this.area.value)){
-      this._makeRed();
-    }
-  };
-  _regExpMail(){
-    //Почта в указанном формате
-    const regex = /^(?!.*@.*@.*$)(?!.*@.*\-\-.*\..*$)(?!.*@.*\-\..*$)(?!.*@.*\-$)(.*@.+(\..{1,11})?)$/gi;
-    if (!regex.test(this.area.value)){
-      this._makeRed();
-    }
-  };
-  _regExpArea(){
-    //Все что угодно, кроме определенных символов, таких так: .,/\ и прочие.
-    //Не получилось сделать проверку на "непустоту" строки
-    const regex = /^(?!.*\..*,.*\/.*\\-.*\+.*\\.*&.*\^.*%.*\$.*#.*@.*!.*&.*.*.*\(.*\).*$)([A-Z]|[a-z]|[А-Я]|[а-я]+)/gi;
-    if (!regex.test(this.area.value)){
-      this._makeRed();
-    }
-  };
-
-  /**
-   * Переключатель, в зависимости от id пришедшего поля вызывает соответствующий приветный метод
-   * @private
-   */
-  _switcher(){
-    switch (this.area.id) {
-      case 'name':
-        this._regExpName(this);
-        break;
-      case 'phone':
-        this._regExpPhone(this);
-        break;
-      case 'email':
-        this._regExpMail(this);
-        break;
-      case 'inputText':
-        this._regExpArea(this);
-        break;
-    }
-  };
-
-  /**
-   * Метод вызывает переключатель
-   * @private
-   */
-  _isFull(){
-    this._switcher();
-  };
-
 }
 
 /**
@@ -92,17 +57,15 @@ class Sender {
    */
   _listener() {
     this.container.addEventListener('click', () => {
-      let inputName = (document.getElementById('name'));
-      let Name = new Inputarea(this.container, inputName);
-      let inputPhone = (document.getElementById('phone'));
-      let Phone = new Inputarea(this.container, inputPhone);
-      let inputEmail = (document.getElementById('email'));
-      let Email = new Inputarea(this.container, inputEmail);
-      let inputText = (document.getElementById('inputText'));
-      let Text = new Inputarea(this.container, inputText);
+      event.preventDefault();
+      let ids = document.querySelectorAll('input');
+      ids.forEach(elem => new Inputarea(this.container, elem));
+      let areas = document.querySelectorAll('textarea');
+      areas.forEach(elem => new Inputarea(this.container, elem));
     });
   }
 }
+
 //Создаем кнопку соответствующего класса
 const toSend = new Sender(document.getElementById('send'));
 
